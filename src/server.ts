4ca45bec312836a9b225a4e8130072e49d829930
guih0113 <gui.henriquess13@gmail.com>
2025-06-19 02:17:43 -0300

    adding port in log for easier viewing

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import { app } from "./app";
 import { env } from "./env";
 
+const port = process.env.PORT;
+
 app
 	.listen({
 		port: env.PORT,
 	})
 	.then(() => {
-		console.log("HTTP server is running!");
+		console.log(`HTTP server is running on port: ${port}`);
 	});
